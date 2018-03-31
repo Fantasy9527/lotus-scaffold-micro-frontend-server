a55bd1c1084732a34ce2faed7a992696f95dace6
yangfan01 <yangfan01@jcgroup.com.cn>
2018-04-01 04:51:48 +0800

    fixed

--- src/services/deploy/deploy.class.js
+++ src/services/deploy/deploy.class.js
@@ -67,7 +67,9 @@ class Service {
       }
       
     }
+
     try {
+      console.log('准备打开路径',originPath)
       exec(`cd ${originPath}`);
     } catch (error) {
       console.log(originPath,'文件夹打开失败')
